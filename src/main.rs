52701d56b82775291e1fee813c7d42dde3d938b5
John Stone <coleslaw.john@gmail.com>
2020-04-15 22:17:17 -0400

    Split up logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let (query, filename) = parse_configs(&args);
+
+    println!("Searching for {}", query);
+    println!("Searching in {}", filename);
+
+    let content = fs::read_to_string(filename).expect("Could not read file");
+    println!("{}", content);
+}
+
+fn parse_configs(args: &[String]) -> (&String, &String) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
