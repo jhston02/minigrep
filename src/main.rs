4c2227436c776ac58271c942c1e244cdf4e37350
John Stone <coleslaw.john@gmail.com>
2020-04-16 20:32:57 -0400

    split functionality into lib folder

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,17 @@
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", config.query);
-    println!("Searching in {}", config.filename);
-
-    let content = fs::read_to_string(config.filename).expect("Could not read file");
-    println!("{}", content);
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguemts");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Config { query, filename }
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error {}", e);
+        process::exit(1);
     }
 }
