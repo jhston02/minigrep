13200bc02c7caea512a27932aa69411cdb647824
John Stone <coleslaw.john@gmail.com>
2020-04-19 10:24:32 -0400

    iterators

--- src/lib.rs
+++ src/lib.rs
@@ -27,50 +27,36 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a filename string")
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config { query, filename, case_sensitive })
     }
 }
 
 fn search<'a>(query:&str, content:&'a str) -> Vec<&'a str> {
-    let mut matches:Vec<&str> = Vec::new();
-    for line in content.lines() {
-        if line.contains(query) {
-            matches.push((line));
-        }
-    }
-    matches
+    content.lines().filter(|line| line.contains(query)).collect()
 }
 
 fn search_case_insensitive<'a>(query:&str, content:&'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut matches:Vec<&str> = Vec::new();
-    for line in content.lines() {
-        if line.to_lowercase().contains(&query) {
-            matches.push((&line));
-        }
-    }
-    matches
+    content.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
 }
 
 #[cfg(test)]
 mod tests {
     use crate::Config;
     use super::*;
-
-    #[test]
-    fn instantiate_config_1_value_errors() {
-        let args = vec!(String::from("Howdy"));
-        if let Err(e) = Config::new(&args) {
-            assert_eq!(e, "not enough arguments");
-        }
-    }
 
     #[test]
     fn case_sensitive() {
